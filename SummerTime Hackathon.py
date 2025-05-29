af60b7b052d2d931a0e97e835e6264d98f1f0dec
gursimransachdeva <sachdeva8150@mydusd.org>
2025-05-29 11:00:45 -0700

    Add files via upload

--- SummerTime Hackathon.py
+++ SummerTime Hackathon.py
@@ -1,4 +1,4 @@
-word_categories = ["adjective", "noun", "verb", "place", "food", "animal", "weather", "emotion"]
+word_categories = ["adjective", "noun", "verb", "place", "food", "animal", "weather", "emotion", "sound", "exclamation"]
 
 def collect_words():
     words = {}
@@ -8,7 +8,13 @@ def collect_words():
     return words
 
 def generate_story(words):
-    story = "One hot summer day, I went to the " + words["place"] + ". The weather was " + words["weather"] + ", and I felt very " + words["emotion"] + ". Suddenly, I saw a " + words["adjective"] + " " + words["animal"] + " sitting on a beach chair. It was " + words["verb"] + " while eating some delicious " + words["food"] + ". I couldn't believe my eyes! I told my friend, and they said, 'Wow, what a " + words["noun"] + " kind of day!'"
+    story = "One " + words["weather"] + " summer day, I went to the " + words["place"] + ". "
+    story = story + "I was feeling " + words["emotion"] + " when suddenly I heard a loud '" + words["sound"] + "'! "
+    story = story + "I turned and saw a " + words["adjective"] + " " + words["animal"] + " wearing sunglasses and sipping on " + words["food"] + ". "
+    story = story + "It was " + words["verb"] + " like it owned the place. "
+    story = story +"I couldn't believe my eyes, so I yelled '" + words["exclamation"] + "'! "
+    story = story + "My friend laughed and said, 'This is the most " + words["noun"] + " kind of summer ever!'"
+
     return story
 
 user_words = collect_words()
